6f0d9fdd14d10ba159bc8949c54e4d348ab05a1f
animek12 <111511065+animek12@users.noreply.github.com>
2022-11-24 14:52:37 +0800

    Update mode-bot.js

--- plugins/mode-bot.js
+++ plugins/mode-bot.js
@@ -17,9 +17,9 @@ contextInfo: { externalAdReply :{
                         mediaType: 2,
                         description: 'anu',
                         title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
-                        body: `Subscribe YT My Bestie`,          previewType: 0,
+                        body: `Website Fin Ofc`,          previewType: 0,
                         thumbnail: await (await fetch(`https://i.ibb.co/jfZVKmC/babi2.jpg`)).buffer(),
-                        sourceUrl: 'https://youtu.be/pwLZpdfO8AU'
+                        sourceUrl: 'https://linktr.ee/fin222'
                       }}
 })
 }
