6c8bf7ef6cba8b3dec0862e583a338a91d87ee1a
animek12 <111511065+animek12@users.noreply.github.com>
2022-11-30 08:41:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Fin Ofc', {
+say('KingOfBear', {
   font: 'chrome',
   align: 'center',
   colors: ['red', 'magenta']
